b364feeb2fdbfee34a541f27aa90ded08de4e143
Daniel Navaez <dnarvaeztavera@gmail.com>
2024-09-10 11:10:54 -0500

    Modificada la migración de usuarios con los campos por defecto de Laravel

--- database/factories/ProductoFactory.php
+++ database/factories/ProductoFactory.php
@@ -11,13 +11,16 @@ class ProductoFactory extends Factory
 {
     /**
      * Define the model's default state.
-     *
+     * 
      * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            //
+            'nombre' => fake()->word(),
+            'descripcion' => fake()->text(),
+            'precio' => fake()->numberBetween(10000, 500000),
+            'stock' => fake()->numberBetween(1,50)
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::apiResource('v1/productos', App\Http\Controllers\Api\V1\ProductoController::class);
